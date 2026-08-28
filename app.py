2b83053e37cc0aa23c4f26800eb21ee87dfa62dc
Pamela Roberta <pamela.robertasc@gmail.com>
2026-08-27 22:15:17 -0300

    Criei a pasta MAE do projeto, o arquivo app.py, as venvs necessarias, o arquivo requirements.txt, bem como as pastas de camadas e os arquivos que precisam ser existentes dentro de cada uma delas.

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return "Olá, mundo!"
+
+@app.route("/sobre")
+def sobre():
+    return "Gestão de Eventos - DW2."
+
+@app.route("/eventos")
+def eventos():
+    return "Lista de eventos aqui."
+
+#Isso é um comentário
+#Agora executamos pelos proximos comandos o servidor web 
+if __name__ == "__main__":
+    app.run(debug=True)
